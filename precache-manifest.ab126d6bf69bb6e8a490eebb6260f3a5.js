1277e614cdb0ddde3e6f65b4346af5007f93bb16
unknown <technicalbesmile@gmail.com>
2021-12-27 22:38:32 +0530

    Updates

---
+++ precache-manifest.ab126d6bf69bb6e8a490eebb6260f3a5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ced93f544c5e7c0e52e",
+    "url": "./static/js/main.7ced93f5.chunk.js"
+  },
+  {
+    "revision": "e4dfb57b5afe986d58f3",
+    "url": "./static/js/1.e4dfb57b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d45bb91b5b9f6fa951c419eedc46316f",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d9635abd74a9dd168326cc4c015546b5.js"
+  "./precache-manifest.ab126d6bf69bb6e8a490eebb6260f3a5.js"
 );
 
 workbox.clientsClaim();
